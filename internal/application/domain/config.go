4186cbfbcb687c030fbf7cf44fe82d340521206d
pablomendezroyo <mendez4a@gmail.com>
2024-11-20 10:27:41 +0100

    remove unused config

--- internal/application/domain/config.go
+++ internal/application/domain/config.go
@@ -4,8 +4,3 @@ type TelegramConfig struct {
 	Token  string `json:"token"`
 	UserID int64  `json:"userId"`
 }
-
-type Config struct {
-	OperatorID string         `json:"operatorId"`
-	Telegram   TelegramConfig `json:"telegram"`
-}
