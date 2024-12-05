b60f94c498488319c1c0e65c6eb0bd51e9178799
Marketen <marcfont12@gmail.com>
2024-12-05 11:18:12 +0100

    undo redundant mutex

--- internal/adapters/storage/exit_requests.go
+++ internal/adapters/storage/exit_requests.go
@@ -38,8 +38,6 @@ func (fs *Storage) SaveExitRequests(operatorID *big.Int, requests domain.ExitReq
 
 // SaveExitRequest saves an individual exit request for a specific operator ID and validator index.
 func (fs *Storage) SaveExitRequest(operatorID *big.Int, validatorIndex string, exitRequest domain.ExitRequest) error {
-	fs.mu.Lock()         // Lock before modifying
-	defer fs.mu.Unlock() // Unlock when done
 
 	db, err := fs.LoadDatabase()
 	if err != nil {
@@ -83,8 +81,6 @@ func (fs *Storage) GetExitRequests(operatorID string) (domain.ExitRequests, erro
 
 // UpdateExitRequestStatus updates the status of a specific exit request for a validator index.
 func (fs *Storage) UpdateExitRequestStatus(operatorID string, validatorIndex string, status domain.ValidatorStatus) error {
-	fs.mu.Lock()         // Lock before modifying
-	defer fs.mu.Unlock() // Unlock when done
 
 	db, err := fs.LoadDatabase()
 	if err != nil {
@@ -112,8 +108,6 @@ func (fs *Storage) UpdateExitRequestStatus(operatorID string, validatorIndex str
 
 // DeleteExitRequest removes an exit request for a specific operator ID and validator index.
 func (fs *Storage) DeleteExitRequest(operatorID string, validatorIndex string) error {
-	fs.mu.Lock()         // Lock before modifying
-	defer fs.mu.Unlock() // Unlock when done
 
 	db, err := fs.LoadDatabase()
 	if err != nil {
