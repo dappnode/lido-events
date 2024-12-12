20bc0c3a5d4c2c17dff836f1cf4b6c85f02a0644
pablomendezroyo <mendez4a@gmail.com>
2024-12-12 17:53:08 +0100

    Send performance notification

--- internal/application/services/loadPendingHashes.go
+++ internal/application/services/loadPendingHashes.go
@@ -2,22 +2,26 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"lido-events/internal/application/domain"
 	"lido-events/internal/application/ports"
 	"lido-events/internal/logger"
+	"math/big"
 	"sync"
 	"time"
 )
 
 type PendingHashesLoader struct {
 	storagePort   ports.StoragePort
+	notifierPort  ports.NotifierPort
 	ipfsPort      ports.IpfsPort
 	servicePrefix string
 }
 
-func NewPendingHashesLoader(storagePort ports.StoragePort, ipfsPort ports.IpfsPort) *PendingHashesLoader {
+func NewPendingHashesLoader(storagePort ports.StoragePort, notifierPort ports.NotifierPort, ipfsPort ports.IpfsPort) *PendingHashesLoader {
 	return &PendingHashesLoader{
 		storagePort,
+		notifierPort,
 		ipfsPort,
 		"PendingHashesLoader",
 	}
@@ -55,7 +59,6 @@ func (phl *PendingHashesLoader) LoadPendingHashesCron(ctx context.Context, inter
 	}
 }
 
-// loadPendingHashes loads all pending hashes from the storage and fetches the corresponding IPFS data
 func (phl *PendingHashesLoader) LoadPendingHashes() error {
 	// Get operator IDs
 	operatorIDs, err := phl.storagePort.GetOperatorIds()
@@ -63,25 +66,23 @@ func (phl *PendingHashesLoader) LoadPendingHashes() error {
 		logger.ErrorWithPrefix(phl.servicePrefix, "Failed to get operator IDs", err)
 		return err
 	}
-	// Skip if there are no operator IDs
 	if len(operatorIDs) == 0 {
 		logger.InfoWithPrefix(phl.servicePrefix, "No operator IDs found; skipping loading pending hashes")
 		return nil
 	}
 
-	// Get all pending hashes from the storage
+	// Get all pending hashes
 	pendingHashes, err := phl.storagePort.GetPendingHashes()
 	if err != nil {
 		logger.ErrorWithPrefix(phl.servicePrefix, "Failed to get pending hashes", err)
 		return err
 	}
-	// Skip if there are no pending hashes
 	if len(pendingHashes) == 0 {
 		logger.InfoWithPrefix(phl.servicePrefix, "No pending hashes found; skipping loading pending hashes")
 		return nil
 	}
 
-	// Fetch and parse IPFS data for each pending hash
+	// Process each pending hash
 	for _, pendingHash := range pendingHashes {
 		logger.DebugWithPrefix(phl.servicePrefix, "Fetching and parsing IPFS data for pending hash %s", pendingHash)
 
@@ -91,37 +92,56 @@ func (phl *PendingHashesLoader) LoadPendingHashes() error {
 			continue
 		}
 
-		// Process each operator ID in the original report
 		for _, operatorID := range operatorIDs {
-			logger.DebugWithPrefix(phl.servicePrefix, "Saving report data for operator ID %s", operatorID.String())
+			logger.DebugWithPrefix(phl.servicePrefix, "Processing data for operator ID %s", operatorID.String())
 
-			// Get the data for the operator ID
 			data, exists := originalReport.Operators[operatorID.String()]
 			if !exists {
-				// Skip if the operator ID is not found in the report
 				logger.WarnWithPrefix(phl.servicePrefix, "Operator ID %s not found in the original report, skipping", operatorID.String())
 				continue
 			}
 
+			// Check performance and send notifications if needed
+			// TODO: only send the notification if the report is from the last 24 hours, consider using the frame to calculate the timestamp
+			phl.CheckAndNotifyPerformance(operatorID, data.Validators, originalReport)
+
+			// Save the report
 			report := domain.Report{
 				Frame:     originalReport.Frame,
 				Data:      data,
 				Threshold: originalReport.Threshold,
 			}
-
-			// Save the report data for the operator ID
 			if err := phl.storagePort.SaveReport(operatorID, report); err != nil {
-				logger.ErrorWithPrefix(phl.servicePrefix, "Failed to save report data for operator ID %s: %v", operatorID.String(), err)
+				logger.ErrorWithPrefix(phl.servicePrefix, "Failed to save report for operator ID %s: %v", operatorID.String(), err)
 				continue
 			}
 		}
 
-		// Remove the pending hash from storage
+		// Remove the pending hash
 		if err := phl.storagePort.DeletePendingHash(pendingHash); err != nil {
 			logger.ErrorWithPrefix(phl.servicePrefix, "Failed to delete pending hash %s: %v", pendingHash, err)
-			continue
 		}
 	}
 
 	return nil
+}
+
+func (phl *PendingHashesLoader) CheckAndNotifyPerformance(operatorID *big.Int, validators map[string]domain.Validator, originalReport domain.OriginalReport) {
+	for validatorID, validator := range validators {
+		if validator.Perf.Assigned > 0 {
+			performance := float64(validator.Perf.Included) / float64(validator.Perf.Assigned) * 100
+			if performance < originalReport.Threshold {
+				message := fmt.Sprintf(
+					"- 🚨 Operator ID: %s, Validator: %s has performance %.2f%% below the threshold %.2f%% in frame %v",
+					operatorID.String(), validatorID, performance, originalReport.Threshold, originalReport.Frame,
+				)
+				logger.WarnWithPrefix(phl.servicePrefix, message)
+
+				// Send notification
+				if err := phl.notifierPort.SendNotification(message); err != nil {
+					logger.ErrorWithPrefix(phl.servicePrefix, "Failed to send notification: %v", err)
+				}
+			}
+		}
+	}
 }
