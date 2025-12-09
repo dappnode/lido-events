a4d13e39373bb235750c8472114ed29f8415a41f
pablomendezroyo <mendez4a@gmail.com>
2025-12-09 10:17:30 +0100

    update relays monitoring cron

--- cmd/main.go
+++ cmd/main.go
@@ -86,7 +86,7 @@ func main() {
 	proxyService.Start(&wg)
 
 	// Start services
-	go relaysCheckerService.StartRelayMonitoringCron(ctx, 24*time.Hour, &wg)
+	go relaysCheckerService.StartRelayMonitoringCron(ctx, 1*time.Hour, &wg)
 	go pendingHashesLoaderService.LoadHashesCron(ctx, 24*time.Hour, &wg)
 	go validatorExitRequestScannerService.ScanExitRequestEventsCron(ctx, 384*time.Second, &wg)
 	go validatorEjectorService.ValidatorEjectorCron(ctx, 64*time.Minute, &wg)
